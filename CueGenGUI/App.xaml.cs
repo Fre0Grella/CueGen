82ecb7bccceeb0d6f87f261cff8c858d65e053a6
Marco Galeri <marco.galeri@studio.unibo.it>
2024-11-12 18:25:46 +0100

    feat: :tada: Getting down the base for the GUI application

---
+++ CueGenGUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CueGenGUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CueGenGUI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CueGen;
+
+namespace CueGenGUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = MainPageViewModel = new MainPageViewModel();
+        }
+
+    }
+
+}
